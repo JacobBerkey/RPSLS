1c371de2697779758b3abed7c5d88d61825a796a
JacobBerkey <jacobberkey@yahoo.com>
2021-09-13 18:15:58 -0400

    AI function writteen and imported into game class

--- AI.py
+++ AI.py
@@ -1,5 +1,8 @@
 from Player import Player
 
 class AI(Player):
-    def __init__(self):
-        pass
+    def __init__(self, lives, type, description):
+        Player.lives = lives
+        Player.type = type
+        Player.description = description
+        
