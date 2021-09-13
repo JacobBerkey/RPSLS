7ad3d85a4dc91b9f80ff518b9c6ade2754eec09f
JacobBerkey <jacobberkey@yahoo.com>
2021-09-13 18:17:15 -0400

    welcome display created

--- Game.py
+++ Game.py
@@ -7,7 +7,12 @@ class Game:
         self.player2 = AI(3, "AI (Computer)", "Computer")
 
     def display_welcome(self):
-        pass
+        print("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
+        player2 = int(input("Will the second player be a human [Press 1] or [Press 2] to play the AI?"))
+        if player2 == 1:
+            self.player2 = Human(3, "Human", "Human 2")
+        if player2 == 2:
+            pass
 
     def player_turn(self):
         pass
