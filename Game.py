f2ba3a4659b96c4260292c8dda62bc3fc3371d07
Gerard Mijares <gerardmijares39@gmail.com>
2021-09-14 21:56:24 +0400

    fixed bug in play_a_round function

--- Game.py
+++ Game.py
@@ -33,87 +33,87 @@ class Game:
                 print(f"Both players selected {player1_gesture}. It's a tie!")
             elif player2_gesture == "Paper":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} wraps around {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
             elif player2_gesture == "Scissors":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} smashes {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
             elif player2_gesture == "Lizard":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} crushes {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
             elif player2_gesture == "Spock":
                 print(f"{self.player2.description} beats {self.player1.description} since {player1_gesture} vaporizes {player2_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
 
         if player1_gesture == "Paper": 
             if player2_gesture == "Paper":
                 print(f"Both players selected {player1_gesture}. It's a tie!")
             elif player2_gesture == "Scissors":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} cuts {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
             elif player2_gesture == "Rock":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} covers {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
             elif player2_gesture == "Lizard":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} eats {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
             elif player2_gesture == "Spock":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} disproves {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
 
         if player1_gesture == "Scissors": 
             if player2_gesture == "Scissors":
                 print(f"Both players selected {player1_gesture}. It's a tie!")
             elif player2_gesture == "Rock":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} crushes {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
             elif player2_gesture == "Paper":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} cuts {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
             elif player2_gesture == "Lizard":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} decapitates {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
             elif player2_gesture == "Spock":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} smashes {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
 
         if player1_gesture == "Lizard": 
             if player2_gesture == "Lizard":
                 print(f"Both players selected {player1_gesture}. It's a tie!")
             elif player2_gesture == "Rock":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} crushes {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
             elif player2_gesture == "Paper":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} eats {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
             elif player2_gesture == "Scissors":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} decapitates {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
             elif player2_gesture == "Spock":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} poisons {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
 
         if player1_gesture == "Spock": 
             if player2_gesture == "Spock":
                 print(f"Both players selected {player1_gesture}. It's a tie!")
             elif player2_gesture == "Rock":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} vaporizes {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
             elif player2_gesture == "Paper":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} disproves {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
             elif player2_gesture == "Scissors":
                 print(f"{self.player1.description} beats {self.player2.description} since {player1_gesture} smashes {player2_gesture}!")
-                self.player2.lives -= self.player2.lives
+                self.player2.lives = self.player2.lives - 1
             elif player2_gesture == "Lizard":
                 print(f"{self.player2.description} beats {self.player1.description} since {player2_gesture} poisons {player1_gesture}!")
-                self.player1.lives -= self.player1.lives
+                self.player1.lives = self.player1.lives - 1
 
         print(f"Player 1 ({self.player1.description}) now has {self.player1.lives} lives left and Player 2 ({self.player2.description}) now has {self.player2.lives} lives left")
 
     def display_winner(self):
         if self.player1.lives == 0 or self.player2.lives == 0:
             if self.player1.lives > self.player2.lives:
-                print('Player 1 wins!!')
+                print(f"Player 1 ({self.player1.description}) wins!!!!")
             else:
-                print('Player 2 wins!')
+                print(f"Player 2 ({self.player1.description}) wins!!!!") 
 
