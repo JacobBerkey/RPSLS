4bb6163c7d05df3a38196dbff9094b31567c61ad
Gerard Mijares <gerardmijares39@gmail.com>
2021-09-14 06:43:44 +0400

    updated Player class with the gesture_list as a class attribute

--- Player.py
+++ Player.py
@@ -1,6 +1,5 @@
 class Player:
-    def __init__(self, gesture_list):
-         self.gesture_list = gesture_list["Rock", "Paper", "Scissors", "Lizard", "Spock"]
-
+    gesture_list = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+    
     
         
